2d83612320ff51212ee84d9247e00072de615556
“Cal-107” <kaly___7@hotmail.it>
2021-10-20 16:33:14 +0200

    add final

--- js/main.js
+++ js/main.js
@@ -1,21 +1,16 @@
 // Pasword Generator (Non sicuro)
 
 // Ottieni nome visitatore
-// - memorizza in variabile
 const firstName = prompt('Qual è il suo nome?')
 console.log(firstName)
 
 // Ottieni cognome visitatore
-// - memorizza in variabile
 const lastName = prompt('Qual è il suo cognome?')
 console.log(lastName)
 
 // Ottieni colore preferito visitatore
-// - memorizza in variabile
 const colorPref = prompt('Qual è il suo colore preferito?')
 console.log(colorPref)
 
 // Mostrare output a schermo
-// - impostare elemento
-// - impostare il valore all'elemento selezionato
-document.getElementById('password').innerHTML = firstName + lastName + colorPref + '21';
+document.getElementById('password').innerHTML = `${firstName}${lastName}${colorPref}21`;

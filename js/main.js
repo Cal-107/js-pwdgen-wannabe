3f2eaa452191739bbd6875dcc047593ef4a6cd3b
“Cal-107” <kaly___7@hotmail.it>
2021-10-20 17:01:02 +0200

    add final fix

--- js/main.js
+++ js/main.js
@@ -2,15 +2,12 @@
 
 // Ottieni nome visitatore
 const firstName = prompt('Qual è il suo nome?')
-console.log(firstName)
 
 // Ottieni cognome visitatore
 const lastName = prompt('Qual è il suo cognome?')
-console.log(lastName)
 
 // Ottieni colore preferito visitatore
 const colorPref = prompt('Qual è il suo colore preferito?')
-console.log(colorPref)
 
 // Mostrare output a schermo
 document.getElementById('password').innerHTML = `${firstName}${lastName}${colorPref}21`;
